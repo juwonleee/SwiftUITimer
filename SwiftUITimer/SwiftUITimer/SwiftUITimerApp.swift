da915ed3320bf0bfc3664b051b65e91f74652527
juwonleee <juwonlee0509@gmail.com>
2024-11-16 13:31:31 +0900

    🎉Setting: Setting Xcode Project

---
+++ SwiftUITimer/SwiftUITimer/SwiftUITimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITimerApp.swift
+//  SwiftUITimer
+//
+//  Created by Lee Juwon on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUITimer/SwiftUITimerTests/SwiftUITimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITimerTests.swift
+//  SwiftUITimerTests
+//
+//  Created by Lee Juwon on 11/16/24.
+//
+
+import Testing
+@testable import SwiftUITimer
+
+struct SwiftUITimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
