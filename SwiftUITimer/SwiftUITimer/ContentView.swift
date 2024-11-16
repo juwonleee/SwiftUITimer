59a71a09362ea067f54a47242288f95c27830b32
juwonleee <juwonlee0509@gmail.com>
2024-11-16 13:56:43 +0900

    🍱Asset: Add Color Asset

--- SwiftUITimer/SwiftUITimer/ContentView.swift
+++ SwiftUITimer/SwiftUITimer/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Circle()
+            .fill(Color("TimerBg"))
+            .frame(width: 273, height: 273)
+
+          HStack {
+            
+          }
         }
         .padding()
     }
